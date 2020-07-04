7ce1e7f0d3115b8be58fd02dc82fb2bc8e953991
e-vents <phipu@windowslive.com>
2020-07-04 12:18:14 +0200

    set up X1

--- using-ts.ts
+++ using-ts.ts
@@ -1,6 +1,7 @@
 const button = document.querySelector("button");
 const input1 = document.getElementById("num1")! as HTMLInputElement;
 const input2 = document.getElementById("num2")! as HTMLInputElement;
+const name1 = "Noëlle";
 
 function add(num1: number, num2: number) {
   return num1 + num2;

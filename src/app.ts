6f42e65013abaea6cf38d591bb80e1c9d11789e5
Philipp Hartmann <phipu@windowslive.com>
2020-09-18 17:20:14 +0200

    last changes regarding decorators video

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,30 @@
+// autobind decorator
+function Autobind(
+    target: any, 
+    methodName: string, 
+    descriptor: PropertyDescriptor
+) { 
+    const originalMehtod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor =  {
+        configurable: true,
+        get() {
+            const boundFn = originalMehtod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+
+//Validation
+
+
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -9,7 +32,51 @@ class ProjectInput {
 
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input';
+
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+        this.configure();
         this.attach();
+    }
+
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+
+        if (
+            validate({value: enteredTitle, required: true, minLength: 5}) &&
+            validate({value: enteredDescription, required: true, minLength: 5})
+            validate({value: enteredPeople, required: true, minLength: 5})
+        ) {
+            alert('invalid input please try again')
+        } else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
+        }
+    }
+
+    private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
+
+    @Autobind
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people);
+            this.clearInputs();
+        }
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler);
     }
 
     private attach() {
